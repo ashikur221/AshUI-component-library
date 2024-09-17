244fbd0e9ab97623151479aae6c84a695b0ef323
ashiku2 <ashikur1602@gmail.com>
2024-09-18 00:15:13 +0600

    Footer added

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Mainlayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            This is footer
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/clientSide/homepage/Banner.jsx
+++ src/pages/clientSide/homepage/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='lg:flex gap-4 justify-center items-center'>
-            <div className="w-1/2">
+        <div className='lg:flex gap-4  justify-center items-center space-y-10'>
+            <div className="lg:w-1/4">
                 <img src="https://res.cloudinary.com/deifi77os/image/upload/v1726391234/AshUI/Homepage/ilg3csbhzu6arwfkq25l.png" alt="" className='' />
             </div>
-            <div className="w-1/2">
-                <p className='text-4xl lg:text-5xl text-white font-bold lg:text-start text-center '>
+            <div className="lg:w-3/4">
+                <p className='text-4xl lg:text-7xl text-white font-bold text-center '>
                     Rapidly build modern websites without ever leaving your HTML, Tailwind.
                 </p>
             </div>
